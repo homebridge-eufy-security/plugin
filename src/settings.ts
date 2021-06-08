4921e08b6a880f15cf66ee0057019c843b02f50a
Sam Emory <samemory85@gmail.com>
2021-06-07 22:59:45 -0500

    simplying plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'EufySecurityControlHomebridgePlugin';
+export const PLATFORM_NAME = 'EufySecurity';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@samemory/homebridge-eufy-security-control';
+export const PLUGIN_NAME = 'homebridge-eufy-security';
