938cddfd3f2a011bab0783f429f2a549f4361f58
adelamarre <adelamarre@secugrator.com>
2021-08-04 20:42:40 +0200

    Fix SmartLock

--- src/accessories/SmartLockAccessory.ts
+++ src/accessories/SmartLockAccessory.ts
@@ -125,7 +125,7 @@ export class SmartLockAccessory extends DeviceAccessory {
 
   private onDeviceLockPushNotification(
     device: Device,
-    lockStatus: Object,
+    lockStatus: boolean,
   ): void {
 
     this.platform.log.debug(this.accessory.displayName, 'Handle Lock Status:  -- ', lockStatus);
