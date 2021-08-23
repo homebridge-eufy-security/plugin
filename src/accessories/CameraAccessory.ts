7756b2bf70464fee7f9296dd9ecf054c25c99837
adelamarre <adelamarre@secugrator.com>
2021-08-23 18:59:37 +0200

    Fixing issue #190 As per @bropat notice, we need to clean function reference which will be deprecated soon (https://github.com/bropat/eufy-security-client/issues/40)

--- src/accessories/CameraAccessory.ts
+++ src/accessories/CameraAccessory.ts
@@ -7,7 +7,7 @@ import { DeviceAccessory } from './Device';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore  
-import { Camera, Device, DeviceType, PropertyValue } from 'eufy-security-client';
+import { Camera, Device, PropertyName, PropertyValue } from 'eufy-security-client';
 import { EufyCameraStreamingDelegate } from './streamingDelegate';
 
 /**
@@ -38,30 +38,26 @@ export class CameraAccessory extends DeviceAccessory {
 
     this.motion_triggered = false;
 
-    try {
-      if (this.platform.config.enableCamera || (typeof this.Camera.isDoorbell === 'function' && this.Camera.isDoorbell())) {
-        this.platform.log.debug(this.accessory.displayName, 'has a camera');
-        try {
-          this.service = this.cameraFunction(accessory);
-          this.MotionService = this.motionFunction(accessory);
+    if (this.platform.config.enableCamera || (typeof this.Camera.isDoorbell === 'function' && this.Camera.isDoorbell())) {
+      this.platform.log.debug(this.accessory.displayName, 'has a camera');
+      try {
+        this.service = this.cameraFunction(accessory);
+        this.MotionService = this.motionFunction(accessory);
 
-          //video stream (work in progress)
+        //video stream (work in progress)
 
-          const delegate = new EufyCameraStreamingDelegate(this.platform, this.Camera);
-          accessory.configureController(delegate.controller);
-        } catch (Error) {
-          this.platform.log.error(this.accessory.displayName, 'raise error to check and attach livestream function.', Error);
-        }
-      } else {
-        this.platform.log.debug(this.accessory.displayName, 'has a motion sensor.');
-        try {
-          this.service = this.motionFunction(accessory);
-        } catch (Error) {
-          this.platform.log.error(this.accessory.displayName, 'raise error to check and attach motion function.', Error);
-        }
+        const delegate = new EufyCameraStreamingDelegate(this.platform, this.Camera);
+        accessory.configureController(delegate.controller);
+      } catch (Error) {
+        this.platform.log.error(this.accessory.displayName, 'raise error to check and attach livestream function.', Error);
       }
-    } catch (Error) {
-      this.platform.log.error(this.accessory.displayName, 'has a motion sensor.', Error);
+    } else {
+      this.platform.log.debug(this.accessory.displayName, 'has a motion sensor.');
+      try {
+        this.service = this.motionFunction(accessory);
+      } catch (Error) {
+        this.platform.log.error(this.accessory.displayName, 'raise error to check and attach motion function.', Error);
+      }
     }
 
     try {
@@ -87,7 +83,7 @@ export class CameraAccessory extends DeviceAccessory {
     } catch (Error) {
       this.platform.log.error(this.accessory.displayName, 'raise error to check and attach a battery.', Error);
     }
-    
+
     try {
       if (typeof this.Camera.isEnabled === 'function') {
         this.platform.log.debug(this.accessory.displayName, 'has a isEnabled, so append switchEnabledService characteristic to him.');
@@ -111,7 +107,7 @@ export class CameraAccessory extends DeviceAccessory {
     } catch (Error) {
       this.platform.log.error(this.accessory.displayName, 'raise error to check and attach switchEnabledService.', Error);
     }
-  
+
     try {
       if (typeof this.Camera.isMotionDetectionEnabled === 'function') {
         this.platform.log.debug(this.accessory.displayName, 'has a isMotionDetectionEnabled, so append switchMotionService characteristic to him.');
@@ -227,18 +223,18 @@ export class CameraAccessory extends DeviceAccessory {
     return service as Service;
   }
 
-  async getCurrentBatteryLevel() {
-    const batteryLevel = this.Camera.getBatteryValue();
-    return batteryLevel.value as number;
-  }
-
   /**
    * Handle requests to get the current value of the "Status Low Battery" characteristic
    */
   async handleBatteryLevelGet(): Promise<CharacteristicValue> {
-    const currentValue = await this.getCurrentBatteryLevel();
-    this.platform.log.debug(this.accessory.displayName, 'Triggered GET BatteryLevel:', currentValue);
-    return currentValue as number;
+    try {
+      const currentValue = this.Camera.getPropertyValue(PropertyName.DeviceBattery);
+      this.platform.log.debug(this.accessory.displayName, 'Triggered GET DeviceBattery:', currentValue);
+      return currentValue.value as number;
+    } catch {
+      this.platform.log.error(this.accessory.displayName, 'handleBatteryLevelGet', 'Wrong return value');
+      return 0;
+    }
   }
 
   async isMotionDetected() {
@@ -249,38 +245,46 @@ export class CameraAccessory extends DeviceAccessory {
   /**
    * Handle requests to get the current value of the 'Security System Current State' characteristic
    */
-  async handleMotionDetectedGet() {
-    const currentValue = await this.isMotionDetected();
-    this.platform.log.debug(this.accessory.displayName, 'Triggered GET MotionDetected:', currentValue);
-    return currentValue as boolean;
+  async handleMotionDetectedGet(): Promise<CharacteristicValue> {
+    try {
+      const currentValue = this.Camera.getPropertyValue(PropertyName.DeviceMotionDetected);
+      this.platform.log.debug(this.accessory.displayName, 'Triggered GET DeviceMotionDetected:', currentValue);
+      return currentValue.value as boolean;
+    } catch {
+      this.platform.log.error(this.accessory.displayName, 'handleMotionDetectedGet', 'Wrong return value');
+      return false;
+    }
   }
 
   private onDeviceMotionDetectedPushNotification(
     device: Device,
     motion: boolean,
   ): void {
-    this.platform.log.debug(this.accessory.displayName, 'Handle Camera motion:  -- ', motion);
-    this.motion_triggered = (this.motion_triggered) ? false : true;
-    this.platform.log.debug(this.accessory.displayName, 'Handle Camera motion:  -- ', this.motion_triggered);
+    this.platform.log.debug(this.accessory.displayName, 'Handle Camera motion:', motion);
     this.service
       .getCharacteristic(this.characteristic.MotionDetected)
-      .updateValue(this.motion_triggered);
+      .updateValue(motion);
   }
 
   /**
    * Handle requests to get the current value of the "On" characteristic
    */
   async handleEnableGet(): Promise<CharacteristicValue> {
-    const currentValue = this.Camera.isEnabled().value;
-    this.platform.log.debug(this.accessory.displayName, 'Triggered GET Enable:', currentValue);
-    return currentValue as boolean;
+    try {
+      const currentValue = this.Camera.getPropertyValue(PropertyName.DeviceEnabled);
+      this.platform.log.debug(this.accessory.displayName, 'Triggered GET DeviceEnabled:', currentValue);
+      return currentValue.value as boolean;
+    } catch {
+      this.platform.log.error(this.accessory.displayName, 'handleEnableGet', 'Wrong return value');
+      return false;
+    }
   }
 
   /**
        * Handle requests to set the "On" characteristic
        */
   async handleEnableSet(value: CharacteristicValue) {
-    this.platform.log.debug(this.accessory.displayName, 'Triggered SET Enable:', value);
+    this.platform.log.debug(this.accessory.displayName, 'Triggered SET DeviceEnabled:', value);
     const station = this.platform.getStationById(this.Camera.getStationSerial());
     station.enableDevice(this.Camera, value as boolean);
   }
@@ -289,16 +293,21 @@ export class CameraAccessory extends DeviceAccessory {
    * Handle requests to get the current value of the "On" characteristic
    */
   async handleMotionOnGet(): Promise<CharacteristicValue> {
-    const currentValue = await this.Camera.isMotionDetectionEnabled().value;
-    this.platform.log.debug(this.accessory.displayName, 'Triggered GET Motion:', currentValue);
-    return currentValue as boolean;
+    try {
+      const currentValue = await this.Camera.getPropertyValue(PropertyName.DeviceMotionDetection);
+      this.platform.log.debug(this.accessory.displayName, 'Triggered GET DeviceMotionDetection:', currentValue);
+      return currentValue.value as boolean;
+    } catch {
+      this.platform.log.error(this.accessory.displayName, 'handleMotionOnGet', 'Wrong return value');
+      return false;
+    }
   }
 
   /**
          * Handle requests to set the "On" characteristic
          */
   async handleMotionOnSet(value: CharacteristicValue) {
-    this.platform.log.debug(this.accessory.displayName, 'Triggered SET Motion:', value);
+    this.platform.log.debug(this.accessory.displayName, 'Triggered SET DeviceMotionDetection:', value);
     const station = this.platform.getStationById(this.Camera.getStationSerial());
     station.setMotionDetection(this.Camera, value as boolean);
   }
