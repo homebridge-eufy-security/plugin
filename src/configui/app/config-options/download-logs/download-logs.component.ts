fafbd6dbc755cc0c45b798be9c5fd1e8e27dd74b
thieren <22128659+thieren@users.noreply.github.com>
2022-07-22 14:11:50 +0200

    update download message

--- src/configui/app/config-options/download-logs/download-logs.component.ts
+++ src/configui/app/config-options/download-logs/download-logs.component.ts
@@ -11,13 +11,13 @@ export class DownloadLogsComponent implements OnInit {
   // TODO: remove lint warnings
   ngOnInit(): void {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    window.homebridge.addEventListener('compressingLogFile', (event: any) => {
+    window.homebridge.addEventListener('downloadLogsFileCount', (event: any) => {
       // eslint-disable-next-line no-console
       console.log(event);
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const data = event['data'] as any;
-      const filename = data['filename'] as string;
-      this.updateDownloadMessage(`Compressing log file: ${event['filename']}...`);
+      const numberOfFiles = data['numberOfFiles'] as number;
+      this.updateDownloadMessage(`Compressing ${numberOfFiles} log files...`);
     });
   }
 
