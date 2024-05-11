a7980f9b6a6c04b678bfd245bcddcdc76fd73b7a
Lenoxys <3996456+lenoxys@users.noreply.github.com>
2024-05-11 14:36:16 +0000

    Prep for Github fillin system

--- src/configui/app/config-options/download-logs/download-logs.component.ts
+++ src/configui/app/config-options/download-logs/download-logs.component.ts
@@ -4,17 +4,20 @@ import { Router, NavigationStart } from '@angular/router';
 
 import { Buffer } from 'buffer';
 import { NgIf } from '@angular/common';
+import { NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-    selector: 'app-download-logs',
-    templateUrl: './download-logs.component.html',
-    styles: [],
-    standalone: true,
-    imports: [NgIf],
+  selector: 'app-download-logs',
+  templateUrl: './download-logs.component.html',
+  styles: [],
+  standalone: true,
+  imports: [NgIf, NgbProgressbar],
 })
 export class DownloadLogsComponent implements OnInit {
 
-  private routeSub: any;
+  progress: number = 0;
+  status: string = '';
+  wait_timer: number = 200;
 
   constructor(
     private router: Router,
@@ -23,7 +26,7 @@ export class DownloadLogsComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.routeSub = this.router.events.subscribe((event) => {
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
         if (this.logFileLocation !== '') {
           // eslint-disable-next-line no-console
@@ -34,10 +37,13 @@ export class DownloadLogsComponent implements OnInit {
       }
     });
 
-    window.homebridge.addEventListener('downloadLogsFileCount', (event: any) => {
+    window.homebridge.addEventListener('downloadLogsProgress', (event: any) => {
       const data = event['data'] as any;
-      const numberOfFiles = data['numberOfFiles'] as number;
-      this.updateDownloadMessage(`Compressing ${numberOfFiles} log files...`);
+      console.log('downloadLogsProgress', data);
+      this.zone.run(() => {
+        this.progress = data['progress'] as number;
+        this.status = data['status'] as string;
+      });
     });
   }
 
@@ -51,16 +57,16 @@ export class DownloadLogsComponent implements OnInit {
   async downloadLogs() {
     try {
       this.isDownloading = true;
-      const bufferData = await window.homebridge.request('/downloadLogs') as {type: string; data: number[]};
+
+      const bufferData = await window.homebridge.request('/downloadLogs') as { type: string; data: number[] };
       const buffer = Buffer.from(bufferData.data);
 
       const file = new File([buffer], 'logs.zip', { type: 'application/zip' });
-      const url= window.URL.createObjectURL(file);
-      
-      this.logFileLocation = url;
-      window.open(this.logFileLocation);
+      const url = window.URL.createObjectURL(file);
 
-      this.updateDownloadMessage(undefined);
+      this.logFileLocation = url;
+      // window.open(this.logFileLocation);
+
       this.hasDownloaded = true;
     } catch (err) {
       // eslint-disable-next-line no-console
@@ -68,21 +74,14 @@ export class DownloadLogsComponent implements OnInit {
 
       const error = err as { message: string };
       this.failed = true;
-      this.updateDownloadMessage(undefined);
       this.failureMessage = `Generating of compressed logs.zip file did not complete: ${error.message}`;
     } finally {
       this.isDownloading = false;
     }
   }
-  
+
   openLink() {
     window.open(this.logFileLocation);
-  }
-
-  private updateDownloadMessage(message?: string) {
-    this.zone.run(() => {
-      this.downloadMessage = message;
-    });
   }
 
 }
