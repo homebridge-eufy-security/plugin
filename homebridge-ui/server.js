300948426e70882e2af71d786a5f2a3c0dc45e3d
Lenoxys <3996456+lenoxys@users.noreply.github.com>
2026-02-15 12:18:06 -0400

    ui: enhance logging configuration with improved templates and sensitive data masking

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -67,37 +67,61 @@ class UiServer extends HomebridgePluginUiServer {
   }
 
   initLogger() {
-    this.log = new TsLogger({
-      name: `[${LIB_VERSION}]`,
-      prettyLogTemplate: '{{name}}\t{{logLevelName}}\t',
-      prettyErrorTemplate: '\n{{errorName}} {{errorMessage}}\nerror stack:\n{{errorStack}}',
-      prettyErrorStackTemplate: '  • {{fileName}}\t{{method}}\n\t{{fileNameWithLine}}',
-      prettyErrorParentNamesSeparator: ':',
-      prettyErrorLoggerNameDelimiter: '\t',
-      stylePrettyLogs: true,
-      minLevel: 2,
-      prettyLogTimeZone: 'local',
-      prettyLogStyles: {
-        logLevelName: {
-          '*': ['bold', 'black', 'bgWhiteBright', 'dim'],
-          SILLY: ['bold', 'white'],
-          TRACE: ['bold', 'whiteBright'],
-          DEBUG: ['bold', 'green'],
-          INFO: ['bold', 'blue'],
-          WARN: ['bold', 'yellow'],
-          ERROR: ['bold', 'red'],
-          FATAL: ['bold', 'redBright'],
+    const logOptions = {
+      name: '[EufySecurity]', // Name prefix for log messages
+      prettyLogTemplate: '[{{mm}}/{{dd}}/{{yyyy}}, {{hh}}:{{MM}}:{{ss}}]\t{{name}}\t{{logLevelName}}\t', // Template for pretty log output
+      prettyErrorTemplate: '\n{{errorName}} {{errorMessage}}\nerror stack:\n{{errorStack}}', // Template for pretty error output
+      prettyErrorStackTemplate: '  • {{fileName}}\t{{method}}\n\t{{fileNameWithLine}}', // Template for error stack trace
+      prettyErrorParentNamesSeparator: '', // Separator for parent names in error messages
+      prettyErrorLoggerNameDelimiter: '\t', // Delimiter for logger name in error messages
+      stylePrettyLogs: true, // Enable styling for logs
+      minLevel: 3, // Minimum log level to display (3 corresponds to INFO)
+      prettyLogTimeZone: 'local', // Time zone for log timestamps
+      prettyLogStyles: { // Styles for different log elements
+        logLevelName: { // Styles for log level names
+          '*': ['bold', 'black', 'bgWhiteBright', 'dim'], // Default style
+          SILLY: ['bold', 'white'], // Style for SILLY level
+          TRACE: ['bold', 'whiteBright'], // Style for TRACE level
+          DEBUG: ['bold', 'green'], // Style for DEBUG level
+          INFO: ['bold', 'blue'], // Style for INFO level
+          WARN: ['bold', 'yellow'], // Style for WARN level
+          ERROR: ['bold', 'red'], // Style for ERROR level
+          FATAL: ['bold', 'redBright'], // Style for FATAL level
         },
-        dateIsoStr: 'gray',
-        filePathWithLine: 'white',
-        name: 'green',
-        nameWithDelimiterPrefix: ['white', 'bold'],
-        nameWithDelimiterSuffix: ['white', 'bold'],
-        errorName: ['bold', 'bgRedBright', 'whiteBright'],
-        fileName: ['yellow'],
+        dateIsoStr: 'gray', // Style for ISO date strings
+        filePathWithLine: 'white', // Style for file paths with line numbers
+        name: 'green', // Style for logger names
+        nameWithDelimiterPrefix: ['white', 'bold'], // Style for logger names with delimiter prefix
+        nameWithDelimiterSuffix: ['white', 'bold'], // Style for logger names with delimiter suffix
+        errorName: ['bold', 'bgRedBright', 'whiteBright'], // Style for error names
+        fileName: ['yellow'], // Style for file names
       },
-    });
-    this.tsLog = new TsLogger({ type: 'hidden', minLevel: 2 });
+      maskValuesOfKeys: [ // Keys whose values should be masked in logs
+        'username',
+        'password',
+        'token',
+        'clientPrivateKey',
+        'private_key',
+        'login_hash',
+        'serverPublicKey',
+        'cloud_token',
+        'refreshToken',
+        'p2p_conn',
+        'app_conn',
+        'address',
+        'latitude',
+        'longitude',
+        'serialnumber',
+        'serialNumber',
+        'stationSerialNumber',
+        'data',
+        'ignoreStations',
+        'ignoreDevices',
+        'pincode',
+      ],
+    };
+    this.log = new TsLogger(logOptions);
+    this.tsLog = new TsLogger({ ...logOptions, type: 'hidden', minLevel: 2 });
   }
 
   initTransportStreams() {
