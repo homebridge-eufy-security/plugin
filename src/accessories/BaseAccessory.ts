8a2aaa5fefa9b0aa569d6648e69487c718294144
Lenoxys <3996456+lenoxys@users.noreply.github.com>
2026-02-20 11:27:16 -0400

    refactor: replace hardcoded UUIDs with SERV constants in safe service lists

--- src/accessories/BaseAccessory.ts
+++ src/accessories/BaseAccessory.ts
@@ -327,12 +327,12 @@ export abstract class BaseAccessory extends EventEmitter {
     // CameraController creates these automatically during configureController()
     // and they are not tracked in servicesInUse.
     const safeServiceUUIDs = [
-      SERV.CameraRTPStreamManagement.UUID,      // 00000110
-      '0000021A-0000-1000-8000-0026BB765291',    // CameraOperatingMode (HKSV)
-      '00000204-0000-1000-8000-0026BB765291',    // CameraRecordingManagement (HKSV)
-      '00000129-0000-1000-8000-0026BB765291',    // DataStreamTransportManagement (HKSV)
-      SERV.Microphone.UUID,                      // 00000112 (created by CameraController)
-      SERV.Speaker.UUID,                         // 00000113 (created by CameraController)
+      SERV.CameraRTPStreamManagement.UUID,
+      SERV.CameraOperatingMode.UUID,
+      SERV.CameraRecordingManagement.UUID,
+      SERV.DataStreamTransportManagement.UUID,
+      SERV.Microphone.UUID,
+      SERV.Speaker.UUID,
     ];
 
     this.accessory.services.forEach((service) => {
